7350215c4bc2b2b62c29e940f2a6dc99c0c99ba9
tangyonghai <tech@geiqin.com>
2026-09-09 08:21:29 +0800

    Create models.go

---
+++ shared/info/models.go
@@ -0,0 +1,43 @@
+package info
+
+type MysqlConfig struct {
+	Host     string `mapstructure:"host" json:"host"`
+	Port     int    `mapstructure:"port" json:"port"`
+	Name     string `mapstructure:"db" json:"db"`
+	User     string `mapstructure:"user" json:"user"`
+	Password string `mapstructure:"password" json:"password"`
+	Salt     string `mapstructure:"salt" json:"salt"`
+	Prefix   string `mapstructure:"prefix" json:"prefix"`
+}
+
+type MongoDBConfig struct {
+	Host       string `mapstructure:"host" json:"host"`
+	Port       int    `mapstructure:"port" json:"port"`
+	Name       string `mapstructure:"db" json:"db"`
+	User       string `mapstructure:"user" json:"user"`
+	Password   string `mapstructure:"password" json:"password"`
+	Collection string `mapstructure:"collection" json:"collection"`
+}
+
+type RabbitMqConfig struct {
+	Host     string `mapstructure:"host" json:"host"`
+	Port     int    `mapstructure:"port" json:"port"`
+	Exchange string `mapstructure:"exchange" json:"exchange"`
+	User     string `mapstructure:"user" json:"user"`
+	Password string `mapstructure:"password" json:"password"`
+}
+
+type MinioConfig struct {
+	Endpoint        string `mapstructure:"endpoint" json:"endpoint"`
+	AccessKeyID     string `mapstructure:"access_key_id" json:"access_key_id"`
+	SecretAccessKey string `mapstructure:"secret_access_key" json:"secret_access_key"`
+	Bucket          string `mapstructure:"bucket" json:"bucket"`
+}
+
+type OtelConfig struct {
+	EndPoint string `mapstructure:"endpoint" json:"endpoint"`
+}
+
+type SrvInfoConfig struct {
+	Name string `mapstructure:"name" json:"name"`
+}
