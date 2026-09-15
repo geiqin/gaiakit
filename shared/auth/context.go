be7fe756631a95f67e11d26527c806f0f7aefc3f
tangyonghai <tech@geiqin.com>
2026-09-16 06:23:45 +0800

    Update context.go

--- shared/auth/context.go
+++ shared/auth/context.go
@@ -3,15 +3,16 @@ package auth
 import (
 	"context"
 
+	"github.com/bytedance/gopkg/cloud/metainfo"
 	"github.com/geiqin/gaiakit/shared/consts"
 	"github.com/geiqin/gaiakit/shared/utils"
 )
 
 //获得当前店铺ID
 func GetStoreId(ctx context.Context) int64 {
-	val := ctx.Value(consts.AccountID)
-	if val != nil {
-		v := utils.StringToInt64(utils.ToString(val))
+	val, ok := metainfo.GetValue(ctx, consts.StoreID)
+	if ok {
+		v := utils.StringToInt64(val)
 		return v
 	}
 	return 0
@@ -19,9 +20,9 @@ func GetStoreId(ctx context.Context) int64 {
 
 //获得登录账号ID
 func GetAccountId(ctx context.Context) int64 {
-	val := ctx.Value(consts.AccountID)
-	if val != nil {
-		v := utils.StringToInt64(utils.ToString(val))
+	val, ok := metainfo.GetValue(ctx, consts.AccountID)
+	if ok {
+		v := utils.StringToInt64(val)
 		return v
 	}
 	return 0
@@ -29,10 +30,9 @@ func GetAccountId(ctx context.Context) int64 {
 
 //获得登录账号类型
 func GetAccountType(ctx context.Context) string {
-	val := ctx.Value(consts.AccountType)
-	if val != nil {
-		v := utils.ToString(val)
-		return v
+	val, ok := metainfo.GetValue(ctx, consts.AccountType)
+	if ok {
+		return val
 	}
 	return ""
 }
@@ -48,6 +48,14 @@ func IsMaster(ctx context.Context) bool {
 func IsStore(ctx context.Context) bool {
 	storeId := GetStoreId(ctx)
 	if storeId > 1 {
+		return true
+	}
+	return false
+}
+
+func IsMember(ctx context.Context) bool {
+	accountType := GetAccountType(ctx)
+	if accountType == consts.MasterMember || accountType == consts.StoreMember {
 		return true
 	}
 	return false
